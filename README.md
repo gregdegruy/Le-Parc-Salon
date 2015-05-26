2cbc197134d1832bf16dd0d42f351eb902223c5a
Gregory John Degruy <gregory.degruy@gmail.com>
2015-05-25 19:51:00 -0500

    Adding files to cloud

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Le-Parc-Salon
+Le Parc Salon
+============
+####Visit Le Parc Salon
+http://xula.edu/planningir/index.php
+
+Le Parc Salon is in New Orleans, LA located at
+
+####Contributors
+* UX Lead - Gregory Degruy
---
+++ web/assets/compass/config.rb
@@ -0,0 +1,25 @@
+=begin
+
+This project uses Compass to organize its css project files
+
+@see
+https://www.jetbrains.com/phpstorm/webhelp/working-with-sass-and-scss-in-compass-projects.html
+
+=end
+
+preferred_syntax = ":scss"
+
+# directory settings
+http_path = "/"
+relative_assests = true
+css_dir = "../css/"
+sass_dir = "sass/"
+javascript_dir = "../js/"
+images_dir = "../images/"
+
+# css output
+output_style = ":expanded"
+#output_style = ":compressed"
+
+# debug comments
+line_comments = true
