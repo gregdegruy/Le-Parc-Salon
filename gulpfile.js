2f7753df69ebc8c68790062ab82d17f291179f88
Gregory John Degruy <gregory.degruy@gmail.com>
2016-01-07 15:31:59 -0800

    Ported grunt task for sass compilation to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+/**
+ * Sass compile tasks 
+ */
+gulp.task('sass', function () {                  
+    gulp.src('./src/development/css/sass/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./src/development/css/'))
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(gulp.dest('./src/production/css/'));
+});
+gulp.task('sass:watch', function () {
+   gulp.watch('./src/development/css/sass/**/*.scss', ['sass']);
+});
+
+/**
+ * Default task
+ */
+gulp.task('default', function () {
   console.log('✩ Greg Universe ✩');
 });
