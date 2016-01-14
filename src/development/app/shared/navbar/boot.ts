06d4edd3acc5c7ad6deaf4d4a948b9f990a69b82
Gregory John Degruy <gregory.degruy@gmail.com>
2016-01-14 02:44:27 -0800

    Adding initial component logic for navbar
    Included angular2 dependencies in index

---
+++ src/development/app/shared/navbar/boot.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from 'angular2/platform/browser'
+import {Navbar} from './navbar.component'
+
+bootstrap(Navbar);
---
+++ src/development/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Mr. Universe</h1>'
+})
+
+export class Navbar { }
