b8b02a3bbf56f43f965ad5a5de71b5f93df57471
Gregory John Degruy <gregory.degruy@gmail.com>
2016-02-09 01:59:19 -0800

    Resolved SystemJS packages path bug
    Bug fix guidance:
    http://stackoverflow.com/questions/34387174/angular-2-quickstart-unexpec
    ted-token
    
    Ignoring node_modules from future commits:
    http://stackoverflow.com/questions/11459475/should-i-check-in-node-modul
    es-to-git-when-creating-a-node-js-app-on-heroku

--- src/development/app/shared/navbar/controls/app.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({
-    selector: 'leparc-navbar',
-    templateUrl: './src/development/app/shared/navbar/views/navbar.html'
-})
-
-export class AppComponent { }
